3dbb9728fea37a0fba8d8eaf6db3c84c91fb1ab2
Chacoon3 <dyzily@foxmail.com>
2023-10-27 17:54:18 -0400

    upd

--- headstart_status_viewer/headstart_status_viewer/settings.py
+++ headstart_status_viewer/headstart_status_viewer/settings.py
@@ -16,7 +16,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -24,10 +23,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rbr@5*yiq6#y4+o2sfotkh97ky8@5x6k1^e@+!yj5z26bq^sh5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get("APP_DEBUG", True))
+DEBUG = os.environ.get("APP_DEBUG", '1') == '1'
+print("DEBUG:", DEBUG)
 
-ALLOWED_HOSTS = []
+APP_HOST = os.environ.get("APP_HOST", None)
+print("APP_HOST:", APP_HOST)
 
+if DEBUG:
+    ALLOWED_HOSTS = ["*"]
+else:
+    if APP_HOST:
+        ALLOWED_HOSTS = [APP_HOST]
+        CSRF_TRUSTED_ORIGINS = [f'http://{APP_HOST}']
 
 # Application definition
 
@@ -96,6 +103,12 @@ else:
                 'driver': 'pymysql',
                 'charset': 'utf8mb4',
             },
+        }
+    }
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
 
