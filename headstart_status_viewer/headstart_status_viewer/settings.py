04dfdd9b0f5ebe9a48aa4f9624baf94c6499cbba
Chacoon3 <dyzily@foxmail.com>
2023-10-27 18:10:53 -0400

    upd

--- headstart_status_viewer/headstart_status_viewer/settings.py
+++ headstart_status_viewer/headstart_status_viewer/settings.py
@@ -105,12 +105,6 @@ else:
             },
         }
     }
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
 
 
 # Password validation
